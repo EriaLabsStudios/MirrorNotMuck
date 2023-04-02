3aaf0d8250efbdac6025692ff251ec28e63306b8
DESKTOP-NA93RH9\david <David Gomez Ruiz>
2023-04-02 13:33:18 +0200

    Update player controller

---
+++ Assets/scripts/LocalPayerController.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using Mirror;
+using UnityEngine;
+
+public class LocalPlayerController : NetworkBehaviour
+{
+
+    [SerializeField] private float lookSpeed = 3f;
+
+
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    private bool groundedPlayer;
+    [SerializeField]
+    private float playerSpeed = 2.0f;
+    private float jumpHeight = 1.0f;
+    private float gravityValue = -9.81f;
+
+
+    public float lookXLimit = 70.0f;
+
+    float rotationX = 0;
+
+    public Camera mainCamera;
+
+
+    private void Start()
+    {
+        Cursor.visible = false;
+        controller = gameObject.GetComponent<CharacterController>();
+
+    }
+    public override  void OnStartLocalPlayer() {
+        mainCamera.gameObject.SetActive(true);
+    }
+
+
+    private void Update()
+    {
+        if (!isLocalPlayer) return;
+
+        groundedPlayer = controller.isGrounded;
+
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+
+        Vector3 move = transform.forward * Input.GetAxis("Vertical") + transform.right * +Input.GetAxis("Horizontal");
+
+
+
+        controller.Move(move  * Time.deltaTime * playerSpeed);
+
+
+        // Changes the height position of the player..
+        if (Input.GetKey(KeyCode.Space) && groundedPlayer)
+        {
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+        }
+
+
+
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
+
+
+
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        transform.Rotate(Vector3.up * mouseX * lookSpeed);
+
+
+
+        Vector3 cameraRotation = mainCamera.transform.rotation.eulerAngles;
+        rotationX += -Input.GetAxis("Mouse Y") * lookSpeed;
+        rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);
+        mainCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
+
+    }
+
+
+}
